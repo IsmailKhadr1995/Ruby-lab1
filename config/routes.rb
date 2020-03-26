e84c985e1f217b588754840ac2ce02253c8cc315
ismailkhadr1995 <ismailkhadr80@yahoo.com>
2020-03-26 14:10:59 +0200

    test

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+   
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'posts/new'
+  # get 'posts/create'
+  # get 'posts/update'
+  # get 'posts/edit'
+  # get 'posts/destroy'
+  resources :posts
+  root to:"posts#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
